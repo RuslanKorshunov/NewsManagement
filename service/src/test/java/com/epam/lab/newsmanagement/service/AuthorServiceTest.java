b082f00e62925a0f1cc3d8eb219acd6488215d37
Ruslan Korshunov <ruslan_korshunov@epam.com>
2020-03-02 15:43:37 +0300

    I've added tests for AuthorService.

--- service/src/test/java/com/epam/lab/newsmanagement/service/AuthorServiceTest.java
+++ service/src/test/java/com/epam/lab/newsmanagement/service/AuthorServiceTest.java
@@ -2,6 +2,9 @@ package com.epam.lab.newsmanagement.service;
 
 import com.epam.lab.newsmanagement.dao.AuthorDao;
 import com.epam.lab.newsmanagement.dto.AuthorDto;
+import com.epam.lab.newsmanagement.entity.Author;
+import com.epam.lab.newsmanagement.exception.DaoException;
+import com.epam.lab.newsmanagement.exception.IncorrectDataException;
 import com.epam.lab.newsmanagement.exception.ServiceException;
 import com.epam.lab.newsmanagement.mapper.AuthorMapper;
 import com.epam.lab.newsmanagement.validator.AuthorValidator;
@@ -18,10 +21,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 public class AuthorServiceTest {
     @Mock
     private AuthorDao dao;
-
     @Mock
     private AuthorValidator validator;
-
     @Mock
     private AuthorMapper mapper;
 
@@ -29,9 +30,14 @@ public class AuthorServiceTest {
     private AuthorService service;
 
     private AuthorDto rightAuthorDtoWithoutId;
-    private AuthorDto rightAuthorDtoResult;
+    private AuthorDto rightAuthorDtoWithId;
+    private AuthorDto wrongAuthorDtoWithoutId;
     private AuthorDto wrongAuthorDto;
     private AuthorDto nullAuthorDto;
+    private Author rightAuthorWithoutId;
+    private Author rightAuthorWithId;
+    private Author wrongAuthorWithoutId;
+    private Author nullAuthor;
 
     @Before
     public void initialize() {
@@ -44,10 +50,13 @@ public class AuthorServiceTest {
         rightAuthorDtoWithoutId.setName(name);
         rightAuthorDtoWithoutId.setSurname(surname);
 
-        rightAuthorDtoResult = new AuthorDto();
-        rightAuthorDtoResult.setId(id);
-        rightAuthorDtoResult.setName(name);
-        rightAuthorDtoResult.setSurname(surname);
+        rightAuthorDtoWithId = new AuthorDto();
+        rightAuthorDtoWithId.setId(id);
+        rightAuthorDtoWithId.setName(name);
+        rightAuthorDtoWithId.setSurname(surname);
+
+        wrongAuthorDtoWithoutId = new AuthorDto();
+        wrongAuthorDtoWithoutId.setName(name);
 
         wrongAuthorDto = new AuthorDto();
         wrongAuthorDto.setId(id);
@@ -57,42 +66,56 @@ public class AuthorServiceTest {
         nullAuthorDto.setId(idNull);
         nullAuthorDto.setName(name);
         nullAuthorDto.setSurname(surname);
+
+        rightAuthorWithoutId = new Author(name, surname);
+
+        rightAuthorWithId = new Author(id, name, surname);
+
+        wrongAuthorWithoutId = new Author();
+        wrongAuthorWithoutId.setName(name);
+
+        nullAuthor = new Author(idNull, name, surname);
     }
 
     @Test
     public void createRightAuthorTest() {
         try {
-            Mockito.when(service.create(rightAuthorDtoWithoutId)).thenReturn(rightAuthorDtoResult);
+            Mockito.when(mapper.toEntity(rightAuthorDtoWithoutId)).thenReturn(rightAuthorWithoutId);
+            Mockito.doNothing().when(validator).validate(rightAuthorWithId);
+            Mockito.when(mapper.toDto(rightAuthorWithId)).thenReturn(rightAuthorDtoWithId);
+            Mockito.when(dao.create(rightAuthorWithoutId)).thenReturn(rightAuthorWithId);
             AuthorDto authorDto = service.create(rightAuthorDtoWithoutId);
-            Assert.assertEquals(rightAuthorDtoResult, authorDto);
-        } catch (ServiceException e) {
+            Assert.assertEquals(rightAuthorDtoWithId, authorDto);
+        } catch (ServiceException | DaoException | IncorrectDataException e) {
             Assert.fail("createRightAuthorTest was failed.");
         }
     }
 
     @Test(expected = ServiceException.class)
-    public void createWrongAuthorTest() throws ServiceException {
-        Mockito.when(service.create(wrongAuthorDto)).thenThrow(ServiceException.class);
-        AuthorDto authorDto = service.create(rightAuthorDtoWithoutId);
-        Assert.fail("addWrongAuthor was failed.");
+    public void createWrongAuthorTest() throws ServiceException, IncorrectDataException {
+        Mockito.when(mapper.toEntity(wrongAuthorDtoWithoutId)).thenReturn(wrongAuthorWithoutId);
+        Mockito.doThrow(ServiceException.class).when(validator).validate(wrongAuthorWithoutId);
+        AuthorDto authorDto = service.create(wrongAuthorDtoWithoutId);
+        Assert.fail("createWrongAuthorTest was failed.");
     }
 
     @Test
     public void readExistingAuthorTest() {
         try {
             long id = 31;
-            Mockito.when(service.read(id)).thenReturn(rightAuthorDtoResult);
+            Mockito.when(dao.read(id)).thenReturn(rightAuthorWithId);
+            Mockito.when(mapper.toDto(rightAuthorWithId)).thenReturn(rightAuthorDtoWithId);
             AuthorDto authorDto = service.read(id);
-            Assert.assertEquals(rightAuthorDtoResult, authorDto);
-        } catch (ServiceException e) {
+            Assert.assertEquals(rightAuthorDtoWithId, authorDto);
+        } catch (ServiceException | DaoException e) {
             Assert.fail("readExistingAuthorTest was failed.");
         }
     }
 
     @Test(expected = ServiceException.class)
-    public void readNullAuthorTest() throws ServiceException {
+    public void readNullAuthorTest() throws ServiceException, DaoException {
         long id = 100;
-        Mockito.when(service.read(id)).thenThrow(ServiceException.class);
+        Mockito.when(dao.read(id)).thenThrow(DaoException.class);
         AuthorDto authorDto = service.read(id);
         Assert.fail("readNullAuthorTest was failed.");
     }
@@ -100,25 +123,30 @@ public class AuthorServiceTest {
     @Test
     public void updateExistingAuthorTest() {
         try {
-            Mockito.when(service.update(rightAuthorDtoResult)).thenReturn(rightAuthorDtoResult);
-            AuthorDto authorDto = service.update(rightAuthorDtoResult);
-            Assert.assertEquals(rightAuthorDtoResult, authorDto);
-        } catch (ServiceException e) {
+            Mockito.when(mapper.toEntity(rightAuthorDtoWithId)).thenReturn(rightAuthorWithId);
+            Mockito.doNothing().when(validator).validate(rightAuthorWithId);
+            Mockito.when(dao.update(rightAuthorWithId)).thenReturn(rightAuthorWithId);
+            AuthorDto authorDto = service.update(rightAuthorDtoWithId);
+            Assert.assertEquals(rightAuthorDtoWithId, authorDto);
+        } catch (ServiceException | IncorrectDataException | DaoException e) {
             Assert.fail("updateExistingAuthorTest was failed.");
         }
     }
 
     @Test(expected = ServiceException.class)
-    public void updateWrongAuthorTest() throws ServiceException {
-        Mockito.when(service.update(wrongAuthorDto)).thenThrow(ServiceException.class);
-        AuthorDto authorDto = service.update(rightAuthorDtoResult);
+    public void updateWrongAuthorTest() throws ServiceException, IncorrectDataException {
+        Mockito.when(mapper.toEntity(wrongAuthorDto)).thenReturn(wrongAuthorWithoutId);
+        Mockito.doThrow(IncorrectDataException.class).when(validator).validate(wrongAuthorWithoutId);
+        AuthorDto authorDto = service.update(wrongAuthorDto);
         Assert.fail("updateWrongAuthorTest was failed.");
     }
 
     @Test(expected = ServiceException.class)
-    public void updateNullAuthorTest() throws ServiceException {
-        Mockito.when(service.update(nullAuthorDto)).thenThrow(ServiceException.class);
-        AuthorDto authorDto = service.update(rightAuthorDtoResult);
+    public void updateNullAuthorTest() throws ServiceException, IncorrectDataException, DaoException {
+        Mockito.when(mapper.toEntity(nullAuthorDto)).thenReturn(nullAuthor);
+        Mockito.doNothing().when(validator).validate(nullAuthor);
+        Mockito.when(dao.update(nullAuthor)).thenThrow(DaoException.class);
+        AuthorDto authorDto = service.update(nullAuthorDto);
         Assert.fail("updateNullAuthorTest was failed.");
     }
 
@@ -126,18 +154,19 @@ public class AuthorServiceTest {
     public void deleteExistingAuthor() {
         try {
             long id = 31;
-            Mockito.when(service.delete(id)).thenReturn(rightAuthorDtoResult);
+            Mockito.when(dao.delete(id)).thenReturn(rightAuthorWithId);
+            Mockito.when(mapper.toDto(rightAuthorWithId)).thenReturn(rightAuthorDtoWithId);
             AuthorDto authorDto = service.delete(id);
-            Assert.assertEquals(rightAuthorDtoResult, authorDto);
-        } catch (ServiceException e) {
+            Assert.assertEquals(rightAuthorDtoWithId, authorDto);
+        } catch (ServiceException | DaoException e) {
             Assert.fail("deleteExistingAuthor was failed.");
         }
     }
 
     @Test(expected = ServiceException.class)
-    public void deleteNullAuthor() throws ServiceException {
+    public void deleteNullAuthor() throws ServiceException, DaoException {
         long id = 100;
-        Mockito.when(service.delete(id)).thenThrow(ServiceException.class);
+        Mockito.when(dao.delete(id)).thenThrow(ServiceException.class);
         AuthorDto authorDto = service.delete(id);
         Assert.fail("deleteNullAuthor was failed.");
     }
